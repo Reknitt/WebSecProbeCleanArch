200b7ef7d27b769bd7e042e1c06350fc4c3c6e03
Pavel <69975860+Reknitt@users.noreply.github.com>
2024-04-03 22:57:49 +0300

    fixed vulnerability delete function

--- Infrastructure/DbContexts/SqliteDbContext.cs
+++ Infrastructure/DbContexts/SqliteDbContext.cs
@@ -9,19 +9,27 @@ namespace WebSecProbeCleanArch.Infrastructure.DbContexts
     {
         public DbSet<User> Users => Set<User>();
         public DbSet<Vulnerability> Vulnerabilities => Set<Vulnerability>();
+        public DbSet<ExampleOfAttack> ExamplesOfAttack => Set<ExampleOfAttack>();
 
         public SqliteDbContext() => Database.EnsureCreated();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=test.db");
+            //optionsBuilder.LogTo(Console.WriteLine);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Vulnerability>()
-                .HasMany(p => p.Attacks)
-                .WithOne(p => p.Vulnerability)
+                .HasMany(p => p.Attacks) // у Vulnerability много сценариев аттак
+                .WithOne(p => p.Vulnerability) // у сценариев атак одна Vulnerability
                 .HasForeignKey(p => p.VulnerabilityId)
+                .OnDelete(DeleteBehavior.Cascade); // каскадное удаление связанных таблиц
+
+            modelBuilder.Entity<ExampleOfAttack>()
+                .HasMany(p => p.StepsOfAttack)
+                .WithOne(p => p.ExampleOfAttack)
+                .HasForeignKey(p => p.ExampleOfAttackId)
                 .OnDelete(DeleteBehavior.Cascade);
         }
     }
